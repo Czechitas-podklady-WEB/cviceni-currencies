2457419dbbe9f0189fc7cfe507f6aa50e7107c29
Filip Jirsák <filip@jirsak.org>
2025-05-09 23:19:44 +0200

    Příklad možného řešení.

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,17 +1,39 @@
-import './style.css';
+import { useEffect, useState } from "react";
+import "./style.css";
 
 const currencies = {
   USD: { CZK: 23.823 },
   EUR: { CZK: 24.74 },
-  GBP: { CZK: 29.067 }
+  GBP: { CZK: 29.067 },
 };
 
-export const Rate = () => {
+export const Rate = ({ from }) => {
+  const [rate, setRate] = useState(null);
+
+  useEffect(() => {
+    const fetchRate = async () => {
+      const resp = await fetch(
+        `https://api.frankfurter.app/latest?from=${from}&to=CZK`
+      );
+      const data = await resp.json();
+      setRate(data.rates.CZK)
+    };
+    fetchRate();
+  }, [from]);
+
   return (
     <div className="rate">
-      <div className="rate__currency">1 USD</div>
+      <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      {/*
+      Řešení kurzovního lístku bez API:
+      <div className="rate__value">{currencies[from].CZK} CZK</div>
+      */}
+      {rate === null ? (
+        "načítám…"
+      ) : (
+        <div className="rate__value">{rate} CZK</div>
+      )}
     </div>
   );
 };
